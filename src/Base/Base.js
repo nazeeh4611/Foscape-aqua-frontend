8e7b9b4d4bbcd57a623cef514cabd6caa054d6d6
Nazeeh Nahaban <nazeehnahaban09@gmail.com>
2026-02-18 14:01:27 +0400

    base changed to railway

--- src/Base/Base.js
+++ src/Base/Base.js
@@ -3,7 +3,8 @@ export const ClientId =
 
 // export const baseurl = `http://localhost:3001/api/`
 
-export const baseurl = `https://foscape-aqua-backend.onrender.com/api/`
+// export const baseurl = `https://foscape-aqua-backend.onrender.com/api/`
+export const baseurl = `foscape-aqua-backend-production.up.railway.app/api/`
 
 
 
